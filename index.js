747ee5af477a6fdd6d52f2b34037fb221cfb2386
Raiyan109 <kraiyan109@gmail.com>
2022-06-12 17:41:11 +0600

    params

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -23,6 +23,13 @@ async function run() {
             const cursor = projectCollection.find(query)
             const projects = await cursor.toArray()
             res.send(projects)
+        })
+
+        app.get('/project/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const details = await projectCollection.findOne(query)
+            res.send(details)
         })
 
     }
